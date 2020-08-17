f2a83200608a64584fc7fc8550106a195bcba2e0
cassidyantic <62527185+cassidyantic@users.noreply.github.com>
2020-08-16 22:57:00 -0400

    icon correction

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import React from 'react'
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <span className="icon fa-cloud"></span>
+      <span className="icon cloud fa-cloud"></span>
     </div>
 
     <div className="content">
